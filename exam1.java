9c298c501fc8b355297047bb2046be98ab5252cf
OverWorkedFeline <115929786+OverWorkedFeline@users.noreply.github.com>
2022-11-17 15:37:39 +0530

    Add files via upload

---
+++ exam1.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class exam1 {
+    public static void main(String[] args) {
+        ArrayList<String> c = new ArrayList();
+        c.add("Mahendra");
+        c.add("Twinkal");
+        c.add("Sumit");
+        c.add("Saurabh");
+        c.add("Pavan");
+        c.add("Atharv");
+        c.add("Aviral");
+        for(String i: c ){
+            System.out.println(i);
+        }
+    }
+}
